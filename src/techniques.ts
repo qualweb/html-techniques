722a441961aca3c69bbff478032ea112ef11cb7d
arbsantos <andre13521@gmail.com>
2019-09-09 15:19:43 +0100

    ported H25 technique -> QW-HTML-T13

--- src/techniques.ts
+++ src/techniques.ts
@@ -1,11 +1,14 @@
 import * as QW_HTML_T1 from './techniques/QW-HTML-T1';
+import * as QW_HTML_T13 from './techniques/QW-HTML-T13';
 
 const techniques = {
-  'QW-HTML-T1': QW_HTML_T1
+  'QW-HTML-T1': QW_HTML_T1,
+  'QW-HTML-T13': QW_HTML_T13
 };
 
 const techniquesToExecute = {
-  'QW-HTML-T1': true
+  'QW-HTML-T1': true,
+  'QW-HTML-T13': true
 };
 
 export {
---
+++ src/techniques/QW-HTML-T13.ts
@@ -0,0 +1,143 @@
+'use strict';
+
+import _ from 'lodash';
+import {
+  HTMLTechnique,
+  HTMLTechniqueResult
+} from '@qualweb/html-techniques';
+import {
+  DomElement
+} from 'htmlparser2';
+
+import {
+  getElementSelector,
+  transform_element_into_html
+} from '../util';
+
+const technique: HTMLTechnique = {
+  name: 'Providing a title using the title element',
+  code: 'QW-HTML-T13',
+  mapping: 'H25',
+  description: 'This technique checks the text alternative of area elements of images maps',
+  metadata: {
+    target: {
+      element: 'title'
+    },
+    'success-criteria': [
+      {
+        name: '2.4.2',
+        level: 'A',
+        principle: 'Operable',
+        url: 'https://www.w3.org/TR/UNDERSTANDING-WCAG20/navigation-mechanisms-title.html'
+      }
+    ],
+    related: ['G88', 'G127'],
+    url: 'https://www.w3.org/TR/WCAG20-TECHS/H25.html',
+    passed: 0,
+    warning: 0,
+    failed: 0,
+    inapplicable: 0,
+    outcome: '',
+    description: ''
+  },
+  results: new Array<HTMLTechniqueResult> ()
+};
+
+function getTechniqueMapping(): string {
+  return technique.mapping;
+}
+
+function hasPrincipleAndLevels(principles: string[], levels: string[]): boolean {
+  let has = false;
+  for (const sc of technique.metadata['success-criteria'] || []) {
+    if (principles.includes(sc.principle) && levels.includes(sc.level)) {
+      has = true;
+    }
+  }
+  return has;
+}
+
+async function execute(element: DomElement | undefined, processedHTML: DomElement[]): Promise < void > {
+
+  const evaluation: HTMLTechniqueResult = {
+    verdict: '',
+    description: ''
+  };
+
+  if (element !== undefined) {
+    if (element.children !== undefined && _.size(element.children) > 0) {
+      const text = element.children[0];
+      if (text['type'] === 'text') { // the title text exists and needs to be verified
+        evaluation.verdict = 'warning';
+        evaluation.description = `Please verify the title text correlates to the page's content`;
+        technique.metadata.warning++;
+      } else { // fails if inside the title tag exists another element instead of text
+        evaluation.verdict = 'failed';
+        evaluation.description = `Title tag contains elements instead of text`;
+        technique.metadata.failed++;
+      }
+    } else { // fails if the title tag is empty
+      evaluation.verdict = 'failed';
+      evaluation.description = `Title text is empty`;
+      technique.metadata.failed++;
+    }
+
+    evaluation.code = transform_element_into_html(element);
+    evaluation.pointer = getElementSelector(element);
+
+  } else { // fails if title element doesn't exist
+    evaluation.verdict = 'failed';
+    evaluation.description = `Title tag doesn't exist`;
+    technique.metadata.failed++;
+  }
+
+
+  technique['data'].push(evaluation);
+
+}
+
+function getFinalResults() {
+  outcomeTechnique();
+  return _.cloneDeep(technique);
+}
+
+function reset(): void {
+  technique.metadata.passed = 0;
+  technique.metadata.warning = 0;
+  technique.metadata.failed = 0;
+  technique.metadata.inapplicable = 0;
+  technique.results = new Array<HTMLTechniqueResult>();
+}
+
+function outcomeTechnique(): void {
+  if (technique.metadata.failed > 0) {
+    technique.metadata.outcome = 'failed';
+  } else if (technique.metadata.warning > 0) {
+    technique.metadata.outcome = 'warning';
+  } else if (technique.metadata.passed > 0) {
+    technique.metadata.outcome = 'passed';
+  } else {
+    technique.metadata.outcome = 'inapplicable';
+  }
+
+  if (technique.results.length > 0) {
+    addDescription();
+  }
+}
+
+function addDescription(): void {
+  for (const result of technique.results || []) {
+    if (result.verdict === technique.metadata.outcome) {
+      technique.metadata.description = result.description;
+      break;
+    }
+  }
+}
+
+export {
+  getTechniqueMapping,
+  hasPrincipleAndLevels,
+  execute,
+  getFinalResults,
+  reset
+};
