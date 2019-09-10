26bc49222b9e7ba0367dc1b08dec4d0dc796e31e
Bruno <brunocea.1@gmail.com>
2019-09-10 15:54:38 +0100

    starting h43

--- src/techniques.ts
+++ src/techniques.ts
@@ -1,11 +1,14 @@
 import * as QW_HTML_T1 from './techniques/QW-HTML-T1';
+import * as QW_HTML_T17 from './techniques/QW-HTML-T17';
 
 const techniques = {
-  'QW-HTML-T1': QW_HTML_T1
+  'QW-HTML-T1': QW_HTML_T1,
+  'QW-HTML-T17': QW_HTML_T17
 };
 
 const techniquesToExecute = {
-  'QW-HTML-T1': true
+  'QW-HTML-T1': true,
+  'QW-HTML-T17': true
 };
 
 export {
---
+++ src/techniques/QW-HTML-T17.ts
@@ -0,0 +1,139 @@
+'use strict';
+
+import _ from 'lodash';
+import {
+  HTMLTechnique,
+  HTMLTechniqueResult
+} from '@qualweb/html-techniques';
+import {
+  DomElement
+} from 'htmlparser2';
+
+import {
+  getElementSelector,
+  transform_element_into_html
+} from '../util';
+
+const technique: HTMLTechnique = {
+  name: 'Using id and headers attributes to associate data cells with header cells in data tables',
+  code: 'QW-HTML-T17',
+  mapping: 'H43',
+  description: 'This technique checks if data cells are associated with header cells in data tables',
+  metadata: {
+    target: {
+      parent: 'table',
+      element: 'td'
+    },
+    'success-criteria': [{
+        name: '1.3.1',
+        level: 'A',
+        principle: 'Perceivable',
+        url: 'https://www.w3.org/TR/2016/NOTE-UNDERSTANDING-WCAG20-20161007/content-structure-separation-programmatic.html'
+      }
+    ],
+    related: [],
+    url: 'https://www.w3.org/TR/WCAG20-TECHS/H43.html',
+    passed: 0,
+    warning: 0,
+    failed: 0,
+    inapplicable: 0,
+    outcome: '',
+    description: ''
+  },
+  results: new Array<HTMLTechniqueResult> ()
+};
+
+function getTechniqueMapping(): string {
+  return technique.mapping;
+}
+
+function hasPrincipleAndLevels(principles: string[], levels: string[]): boolean {
+  let has = false;
+  for (const sc of technique.metadata['success-criteria'] || []) {
+    if (principles.includes(sc.principle) && levels.includes(sc.level)) {
+      has = true;
+    }
+  }
+  return has;
+}
+
+async function execute(element: DomElement | undefined, processedHTML: DomElement[]): Promise < void > {
+
+  if (element === undefined) {
+    return;
+  }
+
+  const evaluation: HTMLTechniqueResult = {
+    verdict: '',
+    description: ''
+  };
+
+  if (element.attribs === undefined) { // fails if the element doesn't contain an alt attribute
+    evaluation.verdict = 'failed';
+    evaluation.description = `The element doesn't contain an alt attribute`;
+    technique.metadata.failed++;
+  } else if (element.attribs['alt'] === undefined) { // fails if the element doesn't contain an alt attribute
+    evaluation.verdict = 'failed';
+    evaluation.description = `The element doesn't contain an alt attribute`;
+    technique.metadata.failed++;
+  } else if (element.attribs['alt'] === '') { // fails if the element's alt attribute value is empty
+    evaluation.verdict = 'failed';
+    evaluation.description = `The element's alt attribute value is empty`;
+    technique.metadata.failed++;
+  } else { // the element contains an non-empty alt attribute, and it's value needs to be verified
+    evaluation.verdict = 'warning';
+    evaluation.description = 'Please verify the alt attribute value describes correctly the correspondent area of the image';
+    technique.metadata.warning++;
+  }
+
+  evaluation.code = transform_element_into_html(element);
+  evaluation.pointer = getElementSelector(element);
+
+  technique.results.push(_.clone(evaluation));
+}
+
+function getFinalResults() {
+  outcomeTechnique();
+  return _.cloneDeep(technique);
+}
+
+function reset(): void {
+  technique.metadata.passed = 0;
+  technique.metadata.warning = 0;
+  technique.metadata.failed = 0;
+  technique.metadata.inapplicable = 0;
+  technique.results = new Array<HTMLTechniqueResult>();
+}
+
+function outcomeTechnique(): void {
+  if (technique.metadata.failed > 0) {
+    technique.metadata.outcome = 'failed';
+  } else if (technique.metadata.warning > 0) {
+    technique.metadata.outcome = 'warning';
+  } else if (technique.metadata.passed > 0) {
+    technique.metadata.outcome = 'passed';
+  } else {
+    technique.metadata.outcome = 'inapplicable';
+  }
+
+  if (technique.results.length > 0) {
+    addDescription();
+  }
+}
+
+function addDescription(): void {
+  for (const result of technique.results || []) {
+    if (result.verdict === technique.metadata.outcome) {
+      technique.metadata.description = result.description;
+      break;
+    }
+  }
+}
+
+export {
+  getTechniqueMapping,
+  hasPrincipleAndLevels,
+  execute,
+  getFinalResults,
+  reset
+};
