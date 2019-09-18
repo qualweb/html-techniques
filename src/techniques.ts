2e1285cb24586dbf4f67d4f741eeba67e3f24bdb
Francisco Caeiro <fc47823@alunos.fc.ul.pt>
2019-09-18 17:19:36 +0100

    remane the technique correcly

--- src/techniques.ts
+++ src/techniques.ts
@@ -1,14 +1,14 @@
 import * as QW_HTML_T1 from './techniques/QW-HTML-T1';
-import * as QW_HTML_T3 from './techniques/QW-HTML-T3';
+import * as QW_HTML_T4 from './techniques/QW-HTML-T4';
 
 const techniques = {
   'QW-HTML-T1': QW_HTML_T1,
-  'QW-HTML-T3': QW_HTML_T3
+  'QW-HTML-T4': QW_HTML_T4
 };
 
 const techniquesToExecute = {
   'QW-HTML-T1': false,
-  'QW-HTML-T3': true
+  'QW-HTML-T4': true
 };
 
 export {
