03474c911698542e452cbc744e94d860f3538819
Francisco Caeiro <fc47823@alunos.fc.ul.pt>
2019-09-18 17:14:49 +0100

    debbuging

--- src/techniques.ts
+++ src/techniques.ts
@@ -1,11 +1,14 @@
 import * as QW_HTML_T1 from './techniques/QW-HTML-T1';
+import * as QW_HTML_T3 from './techniques/QW-HTML-T3';
 
 const techniques = {
-  'QW-HTML-T1': QW_HTML_T1
+  'QW-HTML-T1': QW_HTML_T1,
+  'QW-HTML-T3': QW_HTML_T3
 };
 
 const techniquesToExecute = {
-  'QW-HTML-T1': true
+  'QW-HTML-T1': false,
+  'QW-HTML-T3': true
 };
 
 export {
---
+++ src/techniques/QW-HTML-T3.ts
@@ -0,0 +1,186 @@
+'use strict';
+
+import _ from 'lodash';
+import {
+  HTMLTechnique,
+  HTMLTechniqueResult
+} from '@qualweb/html-techniques';
+import {
+  DomElement
+} from 'htmlparser2';
+
+import {
+  getElementSelector,
+  transform_element_into_html
+} from '../util';
+
+const technique: HTMLTechnique = {
+  name: 'Providing a description for groups of form controls using fieldset and legend elements',
+  code: 'QW-HTML-T3',
+  mapping: 'H71',
+  description: 'This technique checks the correct use of the description element for form controls',
+  metadata: {
+    target: {
+      element: 'fieldset'
+    },
+    'success-criteria': [{
+      name: '1.3.1',
+      level: 'A',
+      principle: 'Perceivable',
+      url: 'https://www.w3.org/WAI/WCAG21/Understanding/info-and-relationships'
+    },
+      {
+      name: '3.3.2',
+      level: 'A',
+      principle: 'Understandable',
+      url: 'https://www.w3.org/WAI/WCAG21/Understanding/labels-or-instructions'
+    },
+    ],
+    related: ['H44', 'H65'],
+    url: 'https://www.w3.org/WAI/WCAG21/Techniques/html/H71',
+    passed: 0,
+    warning: 0,
+    failed: 0,
+    inapplicable: 0,
+    outcome: '',
+    description: ''
+  },
+  results: new Array<HTMLTechniqueResult> ()
+};
+
+function getTechniqueMapping(): string {
+  return technique.mapping;
+}
+
+function hasPrincipleAndLevels(principles: string[], levels: string[]): boolean {
+  let has = false;
+  for (const sc of technique.metadata['success-criteria'] || []) {
+    if (principles.includes(sc.principle) && levels.includes(sc.level)) {
+      has = true;
+    }
+  }
+  return has;
+}
+
+
+
+async function execute(element: DomElement | undefined, processedHTML: DomElement[]): Promise < void > {
+
+  if (element === undefined) {
+    return;
+  }
+
+    const evaluation: HTMLTechniqueResult = {
+      verdict: '',
+      description: '',
+      resultCode: ''
+    };
+
+    if (!verifyFormControl(element)) {
+      evaluation.verdict = 'failed';
+      evaluation.description = 'The fieldset is not in a form control';
+      technique.resultCode = 'RC1';
+      technique.metadata.failed++;
+    }
+    if (!verifyLegendExistence(element)) {
+      evaluation.verdict = 'failed';
+      evaluation.description = 'The legend does not exist in the fieldset element';
+      technique.resultCode = 'RC2';
+      technique.metadata.failed++;
+
+    }
+    if (!verifyLegendContent(element)) {
+      evaluation.verdict = 'failed';
+      evaluation.description = 'The legend is empty';
+      technique.resultCode = 'RC3';
+      technique.metadata.failed++;
+    }
+    if (technique.metadata.failed == 0) {
+      evaluation.verdict = 'warning';
+      evaluation.description = 'Please verify that the legend description is valid';
+      technique.resultCode = 'RC4';
+      technique.metadata.warning++;
+    }
+
+
+    evaluation.htmlCode = transform_element_into_html(element);
+    evaluation.pointer = getElementSelector(element);
+
+    technique.results.push(_.clone(evaluation));
+
+}
+
+function verifyFormControl(elem){
+  const parent = elem.parentNode;
+  return parent == 'form';
+
+}
+
+function verifyLegendExistence(elem){
+  const childs = elem.children;
+  for (let i = 0; i < childs.length; i++){
+    if(childs[i] !== undefined && childs[i].name !== undefined){
+      return childs[i].name === 'legend'
+    }
+  }
+  return false;
+}
+
+function verifyLegendContent(elem){
+  const children = elem.children;
+  if(children !== undefined) {
+    for (let i = 0; i < children.length; i++) {
+      if (children[i] !== undefined && children[i].name === 'legend') {
+        return children[i].children[0].data.trim() === '';
+      }
+    }
+  }
+  return false;
+}
+
+
+function getFinalResults() {
+  outcomeTechnique();
+  return _.cloneDeep(technique);
+}
+
+function reset(): void {
+  technique.metadata.passed = 0;
+  technique.metadata.warning = 0;
+  technique.metadata.failed = 0;
+  technique.metadata.inapplicable = 0;
+  technique.results = new Array<HTMLTechniqueResult>();
+}
+
+function outcomeTechnique(): void {
+  if (technique.metadata.failed > 0) {
+    technique.metadata.outcome = 'failed';
+  } else if (technique.metadata.warning > 0) {
+    technique.metadata.outcome = 'warning';
+  } else if (technique.metadata.passed > 0) {
+    technique.metadata.outcome = 'passed';
+  } else {
+    technique.metadata.outcome = 'inapplicable';
+  }
+
+  if (technique.results.length > 0) {
+    addDescription();
+  }
+}
+
+function addDescription(): void {
+  for (const result of technique.results || []) {
+    if (result.verdict === technique.metadata.outcome) {
+      technique.metadata.description = <string> result.description;
+      break;
+    }
+  }
+}
+
+export {
+  getTechniqueMapping,
+  hasPrincipleAndLevels,
+  execute,
+  getFinalResults,
+  reset
+};
