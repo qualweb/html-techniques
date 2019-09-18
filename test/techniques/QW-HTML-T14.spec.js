4dbeaae4ea2b58a84018ee06ce2662acabb67fd7
Bruno <brunocea.1@gmail.com>
2019-09-18 17:42:20 +0100

    tests done

--- test/techniques/QW-HTML-T14.spec.js
+++ test/techniques/QW-HTML-T14.spec.js
@@ -3,7 +3,50 @@ const {
   executeHTMLT
 } = require('../../dist/index');
 const { expect } = require('chai');
+const {
+  getDom
+} = require('@qualweb/get-dom-puppeteer');
+
 
 describe('Technique QW-HTML-T14', function() {
-  
+
+  const tests = [
+    {
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~bandrade/h35/warning1.html',
+      outcome: 'warning'
+    },
+    {
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~bandrade/h35/failed1.html',
+      outcome: 'failed'
+    },
+    {
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~bandrade/h35/failed2.html',
+      outcome: 'failed'
+    },
+    {
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~bandrade/h35/failed3.html',
+      outcome: 'failed'
+    }
+  ];
+
+  let i = 0;
+  let lastOutcome = 'warning';
+  for (const test of tests || []) {
+    if (test.outcome !== lastOutcome) {
+      lastOutcome = test.outcome;
+      i = 0;
+    }
+    i++;
+    describe(`${test.outcome.charAt(0).toUpperCase() + test.outcome.slice(1)} example ${i}`, function () {
+      it(`should have outcome="${test.outcome}"`, async function () {
+        this.timeout(10 * 1000);
+        const { source, processed } = await getDom(test.url);
+
+
+        const report = await executeHTMLT(source.html.parsed, processed.html.parsed);
+        expect(report.techniques['QW-HTML-T14'].metadata.outcome).to.be.equal(test.outcome);
+      });
+    });
+  }
+
 });
