ca73460d0b14e6617d3355ac3289324c7435a64e
João Afonso Vicente <joao.vicente@campus.ul.pt>
2019-09-20 15:06:03 +0100

    G140 - QW-HTML-T22 - implementation done

---
+++ src/techniques/QW-HTML-T22.ts
@@ -0,0 +1,90 @@
+'use strict';
+
+import _ from 'lodash';
+import {
+  HTMLTechnique,
+  HTMLTechniqueResult
+} from '@qualweb/html-techniques';
+import {
+  DomElement
+} from 'htmlparser2';
+
+import {
+  getElementSelector,
+  transform_element_into_html
+} from '../util';
+import Technique from './Technique.object';
+
+const technique: HTMLTechnique = {
+  name: 'Separating information and structure from presentation to enable different presentations',
+  code: 'QW-HTML-T22',
+  mapping: 'G140',
+  description: 'This technique checks that no attributes are being used to control the visual text presentation',
+  metadata: {
+    target: {
+      element: ['text', 'vlink', 'alink', 'link']
+    },
+    'success-criteria': [{
+        name: '1.3.1',
+        level: 'A',
+        principle: 'Perceivable',
+        url: 'https://www.w3.org/WAI/WCAG21/Understanding/info-and-relationships'
+      },
+      {
+        name: '1.4.5',
+        level: 'AA',
+        principle: 'Perceivable',
+        url: 'https://www.w3.org/WAI/WCAG21/Understanding/images-of-text'
+      },
+      {
+        name: '1.4.9',
+        level: 'AAA',
+        principle: 'Perceivable',
+        url: 'https://www.w3.org/WAI/WCAG21/Understanding/images-of-text-no-exception'
+      }
+    ],
+    related: ['C29'],
+    url: 'https://www.w3.org/WAI/WCAG21/Techniques/general/G140',
+    passed: 0,
+    warning: 0,
+    failed: 0,
+    inapplicable: 0,
+    outcome: '',
+    description: ''
+  },
+  results: new Array<HTMLTechniqueResult> ()
+};
+
+class QW_HTML_T1 extends Technique {
+
+  constructor() {
+    super(technique);
+  }
+
+  async execute(element: DomElement | undefined): Promise<void> {
+
+    const evaluation: HTMLTechniqueResult = {
+      verdict: '',
+      description: '',
+      resultCode: ''
+    };
+
+    if (element === undefined) {
+      evaluation.verdict = 'passed';
+      evaluation.description = `The webpage doesn't use attributes to control the visual text presentation`;
+      evaluation.resultCode = 'RC1';
+    } else {
+      evaluation.verdict = 'failed';
+      evaluation.description = `The webpage uses the attribute ${element.name} to control the visual text presentation`;
+      evaluation.resultCode = 'RC2';
+
+      evaluation.htmlCode = transform_element_into_html(element);
+      evaluation.pointer = getElementSelector(element);
+      evaluation.attributes = element.attribs ? _.intersection(Object.keys(element.attribs), ['text', 'vlink', 'alink', 'link']) : undefined;
+    }
+
+    super.addEvaluationResult(evaluation);
+  }
+}
+
+export = QW_HTML_T1;
