77f449acb1938478baf766b5efa89596ed2ba5cf
João Afonso Vicente <joao.vicente@campus.ul.pt>
2019-09-20 14:54:21 +0100

    G115 - QW-HTML-T21 implementation done

--- src/techniques.ts
+++ src/techniques.ts
@@ -1,11 +1,14 @@
 import QW_HTML_T1 from './techniques/QW-HTML-T1';
+import QW_HTML_T21 from './techniques/QW-HTML-T21';
 
 const techniques = {
-  'QW-HTML-T1': new QW_HTML_T1()
+  'QW-HTML-T1': new QW_HTML_T1(),
+  'QW-HTML-T21': new QW_HTML_T21()
 };
 
 const techniquesToExecute = {
-  'QW-HTML-T1': true
+  'QW-HTML-T1': true,
+  'QW-HTML-T21': true
 };
 
 export {
