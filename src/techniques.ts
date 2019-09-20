4917c0f947d699620679e46ffe7f4cf77e630092
Bruno Andrade <brunocea.1@gmail.com>
2019-09-20 19:16:22 +0100

    test all done

--- src/techniques.ts
+++ src/techniques.ts
@@ -1,11 +1,14 @@
 import QW_HTML_T1 from './techniques/QW-HTML-T1';
+import QW_HTML_T23 from './techniques/QW-HTML-T23';
 
 const techniques = {
-  'QW-HTML-T1': new QW_HTML_T1()
+  'QW-HTML-T1': new QW_HTML_T1(),
+  'QW-HTML-T23': new QW_HTML_T23()
 };
 
 const techniquesToExecute = {
-  'QW-HTML-T1': true
+  'QW-HTML-T1': true,
+  'QW-HTML-T23': true
 };
 
 export {
