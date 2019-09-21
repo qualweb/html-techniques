1f004f92e43ade3acf2c1fd3b028d11c2f062e45
Estriga <fc47839@alunos.fc.ul.pt>
2019-09-21 19:44:50 +0100

    done

--- src/techniques.ts
+++ src/techniques.ts
@@ -1,11 +1,15 @@
 import QW_HTML_T1 from './techniques/QW-HTML-T1';
+import QW_HTML_T19 from './techniques/QW-HTML-T19';
+
 
 const techniques = {
-  'QW-HTML-T1': new QW_HTML_T1()
+  'QW-HTML-T1': new QW_HTML_T1(),
+  'QW-HTML-T19': new QW_HTML_T19()
 };
 
 const techniquesToExecute = {
-  'QW-HTML-T1': true
+  'QW-HTML-T1': true,
+  'QW-HTML-T19': true
 };
 
 export {
--- src/techniques/QW-HTML-T19.ts
+++ src/techniques/QW-HTML-T19.ts
@@ -26,27 +26,22 @@ const technique: HTMLTechnique = {
       parent: 'map',
       element: 'area'
     },
-    'success-criteria': [{
-        name: '1.1.1',
-        level: 'A',
-        principle: 'Perceivable',
-        url: 'https://www.w3.org/TR/UNDERSTANDING-WCAG20/text-equiv-all.html'
-      },
-      {
-        name: '2.4.4',
-        level: 'A',
-        principle: 'Operable',
-        url: 'https://www.w3.org/TR/UNDERSTANDING-WCAG20/navigation-mechanisms-refs.html'
-      },
-      {
-        name: '2.4.9',
-        level: 'AAA',
-        principle: 'Operable',
-        url: 'https://www.w3.org/TR/UNDERSTANDING-WCAG20/navigation-mechanisms-link.html'
-      }
+    'success-criteria': [
+    {
+      name: '2.4.5',
+      level: 'AA',
+      principle: 'Operable',
+      url: 'https://www.w3.org/TR/2016/NOTE-UNDERSTANDING-WCAG20-20161007/navigation-mechanisms-mult-loc.html'
+    },
+    {
+      name: '2.4.8',
+      level: 'AAA',
+      principle: 'Operable',
+      url: 'https://www.w3.org/TR/2016/NOTE-UNDERSTANDING-WCAG20-20161007/navigation-mechanisms-location.html'
+    }
     ],
-    related: ['G91', 'H30'],
-    url: 'https://www.w3.org/TR/WCAG20-TECHS/H24.html',
+    related: ['G1','G63','G64', 'G123'],
+    url: 'https://www.w3.org/TR/WCAG20-TECHS/H59.html',
     passed: 0,
     warning: 0,
     failed: 0,
@@ -54,10 +49,10 @@ const technique: HTMLTechnique = {
     outcome: '',
     description: ''
   },
-  results: new Array<HTMLTechniqueResult> ()
+  results: new Array<HTMLTechniqueResult>()
 };
 
-class QW_HTML_T1 extends Technique {
+class QW_HTML_T19 extends Technique {
 
   constructor() {
     super(technique);
@@ -65,7 +60,7 @@ class QW_HTML_T1 extends Technique {
 
   async execute(element: DomElement | undefined, processedHTML: DomElement[]): Promise<void> {
 
-    if (element === undefined) {
+    if (!element || !element.parent) {
       return;
     }
 
@@ -74,23 +69,30 @@ class QW_HTML_T1 extends Technique {
       description: '',
       resultCode: ''
     };
+    let parentName = element.parent["name"];
 
-    if (!element.attribs) { // fails if the element doesn't contain an alt attribute
+    if (parentName != "head") {
+      evaluation.verdict = 'failed';
+      evaluation.description = `The element is not contained in the head element`;
+      technique.metadata.failed++;
+    }
+    else if (!element.attribs) { // fails if the element doesn't contain an alt attribute
       evaluation.verdict = 'failed';
       evaluation.description = `The element doesn't contain a rel or an href attribute`;
+      technique.metadata.failed++;
     } else {
-      let rel =element.attribs["rel"];
-      let href =element.attribs["href"];
+      let rel = element.attribs["rel"];
+      let href = element.attribs["href"];
 
-      if(!rel){
-        evaluation.verdict = 'failed';
-        evaluation.description = `The element doesn't contain a rel attribute`;
-        technique.metadata.failed++;
-      }else if(!href){
-        evaluation.verdict = 'failed';
-        evaluation.description = `The element doesn't contain an href attribute`;
-        technique.metadata.failed++;
-      }else{
+      if (!rel) {
+        evaluation.verdict = 'warning';
+        evaluation.description = `The element doesn't contain a rel attribute check if this element pertains navigation `;
+        technique.metadata.warning++;
+      } else if (!href) {
+        evaluation.verdict = 'warning';
+        evaluation.description = `The element doesn't contain an href attribute check if this element pertains navigation`;
+        technique.metadata.warning++;
+      } else {
         evaluation.verdict = 'warning';
         evaluation.description = 'The element contains a rel and an href attribute that should be manually verified';
         technique.metadata.warning++;
@@ -100,10 +102,10 @@ class QW_HTML_T1 extends Technique {
     }
     evaluation.htmlCode = transform_element_into_html(element);
     evaluation.pointer = getElementSelector(element);
-    
+
 
     super.addEvaluationResult(evaluation);
   }
 }
 
-export = QW_HTML_T1;
+export = QW_HTML_T19;
