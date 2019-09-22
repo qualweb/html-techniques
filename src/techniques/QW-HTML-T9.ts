ee2ea709b716e53c622f2bc4aea17a06e5547489
Bruno Andrade <brunocea.1@gmail.com>
2019-09-22 14:40:16 +0100

    modifications from Estriga yet to arrive

--- src/techniques/QW-HTML-T9.ts
+++ src/techniques/QW-HTML-T9.ts
@@ -2,59 +2,51 @@
 
 import _ from 'lodash';
 import {
-    HTMLTechnique,
-    HTMLTechniqueResult
+  HTMLTechnique,
+  HTMLTechniqueResult
 } from '@qualweb/html-techniques';
 import {
-    DomElement
+  DomElement
 } from 'htmlparser2';
 
 import {
-    getElementSelector,
-    transform_element_into_html
+  getElementSelector,
+  transform_element_into_html
 } from '../util';
 import Technique from './Technique.object';
 
 const technique: HTMLTechnique = {
-    name: 'Providing text alternatives for the area elements of image maps',
-    code: 'QW-HTML-T1',
-    mapping: 'H24',
-    description: 'This technique checks the text alternative of area elements of images maps',
-    metadata: {import QW_HTML_T1 from './techniques/QW-HTML-T1';
-        target: {
-            'parent-sibling': 'img',
-            parent: 'map',
-            element: 'area'
-        },
-        'success-criteria': [{
-            name: '1.1.1',
-            level: 'A',
-            principle: 'Perceivable',
-            url: 'https://www.w3.org/TR/UNDERSTANDING-WCAG20/text-equiv-all.html'
-        },
-            {
-                name: '2.4.4',
-                level: 'A',
-                principle: 'Operable',
-                url: 'https://www.w3.org/TR/UNDERSTANDING-WCAG20/navigation-mechanisms-refs.html'
-            },
-            {
-                name: '2.4.9',
-                level: 'AAA',
-                principle: 'Operable',
-                url: 'https://www.w3.org/TR/UNDERSTANDING-WCAG20/navigation-mechanisms-link.html'
-            }
-        ],
-        related: ['G91', 'H30'],
-        url: 'https://www.w3.org/TR/WCAG20-TECHS/H24.html',
-        passed: 0,
-        warning: 0,
-        failed: 0,
-        inapplicable: 0,
-        outcome: '',
-        description: ''
+  name: 'Organizing a page using headings',
+  code: 'QW-HTML-T9',
+  mapping: 'G141',
+  description: 'The objective of this technique is to ensure that sections have headings that identify them and that the heading are used in the correct order',
+  metadata: {
+    target: {
+      element: '*'
     },
-    results: new Array<HTMLTechniqueResult>()
+    'success-criteria': [{
+      name: '1.3.1',
+      level: 'A',
+      principle: 'Perceivable',
+      url: 'https://www.w3.org/WAI/WCAG21/Understanding/info-and-relationships'
+    },
+      {
+        name: '2.4.10',
+        level: 'AAA',
+        principle: 'Operable',
+        url: 'https://www.w3.org/WAI/WCAG21/Understanding/section-headings'
+      }
+    ],
+    related: ['G91', 'H30'],
+    url: 'https://www.w3.org/WAI/WCAG21/Techniques/general/G141',
+    passed: 0,
+    warning: 0,
+    failed: 0,
+    inapplicable: 0,
+    outcome: '',
+    description: ''
+  },
+  results: new Array<HTMLTechniqueResult>()
 };
 
 class QW_HTML_T9 extends Technique {
@@ -64,74 +56,65 @@ class QW_HTML_T9 extends Technique {
   }
 
   async execute(element: DomElement | undefined, processedHTML: DomElement[]): Promise<void> {
-    
+
     if (element === undefined || element.children === undefined) {
-        return;
+      return;
     }
 
     const evaluation: HTMLTechniqueResult = {
-        verdict: '',
-        description: '',
-        resultCode:''
+      verdict: '',
+      description: '',
+      resultCode: ''
     };
+
     let regexp = new RegExp('^h[1-6]$');
     let name;
     let list: number[] = [];
     let split;
 
     for (let child of element.children) {
-        name = child["name"];
-        console.log(name);
-        if (name !== undefined && regexp.test(name)) {
-            split = name.split("h")
-            list.push(split[1]);
-        }
-
+      name = child["name"];
+      if (name !== undefined && regexp.test(name)) {
+        split = name.split("h");
+        list.push(split[1]);
+      }
     }
 
-    console.log(list);
     if (list.length === 0) {
-        return;//no H elements
+      return; // no heading elements
     }
+
     let sortedArray: number[] = list.sort((n1, n2) => n1 - n2);
     let equal = true;
     let complete = true;
-    console.log(sortedArray[0]);
 
     for (let i = 0; i < list.length; i++) {
-        if (list[i] !== sortedArray[i])
-            equal = false;
-        if (i > 0 && i - 1 < list.length && sortedArray[i] - sortedArray[i - 1] > 1){
-            complete = false; }
+      if (list[i] !== sortedArray[i])
+        equal = false;
+      if (i > 0 && i - 1 < list.length && sortedArray[i] - sortedArray[i - 1] > 1) {
+        complete = false;
+      }
     }
 
-
-
-    if (!equal) { // fails if the headers arent in the correct order
-        evaluation.verdict = 'failed';
-        evaluation.description = `Headers are not in the correct order`;
-        technique.metadata.failed++;
+    if (!equal) { // fails if the headings aren't in the correct order
+      evaluation.verdict = 'failed';
+      evaluation.description = `Headings are not in the correct order`;
+      technique.metadata.failed++;
     } else if (!complete) { // fails if a header number is missing
-        evaluation.verdict = 'failed';
-        evaluation.description = `Header number is missing`;
-        technique.metadata.failed++;
-    } else { // the H elements are correctly used
-        evaluation.verdict = 'warning';
-        evaluation.description = 'Please verify that headers are used to divide the page correctly';
-        technique.metadata.passed++;
+      evaluation.verdict = 'failed';
+      evaluation.description = `Heading number is missing`;
+      technique.metadata.failed++;
+    } else { // the heading elements are correctly used
+      evaluation.verdict = 'warning';
+      evaluation.description = 'Please verify that headers are used to divide the page correctly';
+      technique.metadata.warning++;
     }
-
-    console.log( evaluation.verdict);
-    console.log( evaluation.description);
 
     evaluation.htmlCode = transform_element_into_html(element);
     evaluation.pointer = getElementSelector(element);
 
     super.addEvaluationResult(evaluation);
-}}
-
-
+  }
+}
 
 export = QW_HTML_T9;
-
-
---
+++ test/techniques/QW-HTML-T9.spec.js
@@ -0,0 +1,77 @@
+
+const {
+  configure,
+  executeHTMLT
+} = require('../../dist/index');
+const { expect } = require('chai');
+const stew = new (require('stew-select')).Stew();
+const {
+  getDom
+} = require('@qualweb/get-dom-puppeteer');
+
+
+describe('Technique QW-HTML-T9', function() {
+
+  const tests = [
+    {
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~bandrade/g141/failed1.html',
+      outcome: 'failed'
+    },
+    {
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~bandrade/g141/failed2.html',
+      outcome: 'failed'
+    },
+    {
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~bandrade/g141/failed3.html',
+      outcome: 'failed'
+    },
+    {
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~bandrade/g141/failed4.html',
+      outcome: 'failed'
+    },
+    {
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~bandrade/g141/failed5.html',
+      outcome: 'failed'
+    },
+    {
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~bandrade/g141/failed6.html',
+      outcome: 'failed'
+    },
+    {
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~bandrade/g141/failed7.html',
+      outcome: 'failed'
+    },
+    {
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~bandrade/g141/inapplicable.html',
+      outcome: 'inapplicable'
+    },
+    {
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~bandrade/g141/warning1.html',
+      outcome: 'warning'
+    }
+  ];
+
+  let i = 0;
+  let lastOutcome = 'failed';
+  for (const test of tests || []) {
+    if (test.outcome !== lastOutcome) {
+      lastOutcome = test.outcome;
+      i = 0;
+    }
+    i++;
+    describe(`${test.outcome.charAt(0).toUpperCase() + test.outcome.slice(1)} example ${i}`, function () {
+      it(`should have outcome="${test.outcome}"`, async function () {
+        this.timeout(10 * 1000);
+
+
+
+        const { source, processed } = await getDom(test.url);
+
+
+        const report = await executeHTMLT(source.html.parsed, processed.html.parsed);
+        expect(report.techniques['QW-HTML-T9'].metadata.outcome).to.be.equal(test.outcome);
+      });
+    });
+  }
+
+});
