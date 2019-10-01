83102ef382e212d2a1267940181171cd8a723e46
aestriga <aestriga@lasige.di.fc.ul.pt>
2019-10-01 17:59:04 +0100

    merged T6

--- src/techniques.ts
+++ src/techniques.ts
@@ -2,12 +2,14 @@
 import QW_HTML_T1 from './techniques/QW-HTML-T1';
 import QW_HTML_T30 from './techniques/QW-HTML-T30';
 import QW_HTML_T5 from './techniques/QW-HTML-T5';
+import QW_HTML_T6 from './techniques/QW-HTML-T6';
 import QW_HTML_T32 from './techniques/QW-HTML-T32';
 
 const techniques = {
   'QW-HTML-T1': new QW_HTML_T1(),
   'QW-HTML-T32': new QW_HTML_T32(),
   'QW-HTML-T5': new QW_HTML_T5(),
+  'QW-HTML-T6': new QW_HTML_T6(),
   'QW-HTML-T30': new QW_HTML_T30()
 };
 
@@ -15,6 +17,7 @@ const techniquesToExecute = {
   'QW-HTML-T1': false,
   'QW-HTML-T32': true,
   'QW-HTML-T5': true,
+  'QW-HTML-T6': true,
   'QW-HTML-T30': true
 };
 
--- src/techniques/QW-HTML-T6.ts
+++ src/techniques/QW-HTML-T6.ts
@@ -13,6 +13,7 @@ import {
   getElementSelector,
   transform_element_into_html
 } from '../util';
+import Technique from "./Technique.object";
 
 const technique: HTMLTechnique = {
   name: 'Using both keyboard and other device-specific functions',
@@ -43,22 +44,14 @@ const technique: HTMLTechnique = {
   },
   results: new Array<HTMLTechniqueResult>()
 };
+class QW_HTML_T6 extends Technique {
 
-function getTechniqueMapping(): string {
-  return technique.mapping;
-}
-
-function hasPrincipleAndLevels(principles: string[], levels: string[]): boolean {
-  let has = false;
-  for (const sc of technique.metadata['success-criteria'] || []) {
-    if (principles.includes(sc.principle) && levels.includes(sc.level)) {
-      has = true;
-    }
+  constructor() {
+    super(technique);
   }
-  return has;
-}
 
-async function execute(element: DomElement | undefined, processedHTML: DomElement[]): Promise<void> {
+
+  async execute(element: DomElement | undefined, processedHTML: DomElement[]): Promise<void> {
 
   if (element === undefined || element.attribs === undefined) {
     return;
@@ -66,7 +59,8 @@ async function execute(element: DomElement | undefined, processedHTML: DomElemen
 
   let evaluation: HTMLTechniqueResult = {
     verdict: '',
-    description: ''
+    description: '',
+    resultCode: ''
   };
 
 
@@ -98,54 +92,12 @@ async function execute(element: DomElement | undefined, processedHTML: DomElemen
   }
 
 
-  evaluation.code = transform_element_into_html(element);
+  evaluation.htmlCode = transform_element_into_html(element);
   evaluation.pointer = getElementSelector(element);
 
   technique.results.push(_.clone(evaluation));
 }
 
-function getFinalResults() {
-  outcomeTechnique();
-  return _.cloneDeep(technique);
 }
 
-function reset(): void {
-  technique.metadata.passed = 0;
-  technique.metadata.warning = 0;
-  technique.metadata.failed = 0;
-  technique.metadata.inapplicable = 0;
-  technique.results = new Array<HTMLTechniqueResult>();
-}
-
-function outcomeTechnique(): void {
-  if (technique.metadata.failed > 0) {
-    technique.metadata.outcome = 'failed';
-  } else if (technique.metadata.warning > 0) {
-    technique.metadata.outcome = 'warning';
-  } else if (technique.metadata.passed > 0) {
-    technique.metadata.outcome = 'passed';
-  } else {
-    technique.metadata.outcome = 'inapplicable';
-  }
-
-  if (technique.results.length > 0) {
-    addDescription();
-  }
-}
-
-function addDescription(): void {
-  for (const result of technique.results || []) {
-    if (result.verdict === technique.metadata.outcome) {
-      technique.metadata.description = result.description;
-      break;
-    }
-  }
-}
-
-export {
-  getTechniqueMapping,
-  hasPrincipleAndLevels,
-  execute,
-  getFinalResults,
-  reset
-};
+export = QW_HTML_T6;
