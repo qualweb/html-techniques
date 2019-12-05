a21abb88866643ef8ba6bd7c38b533898d8d6c4e
João Vicente <joao.vicente@campus.ul.pt>
2019-12-05 16:54:26 +0000

    updated QW-HTML-T19 technique

--- src/techniques/QW-HTML-T19.ts
+++ src/techniques/QW-HTML-T19.ts
@@ -15,15 +15,13 @@ import {
 import Technique from './Technique.object';
 
 const technique: HTMLTechnique = {
-  name: 'Using the link element and navigation tools\n',
-  code: 'QW-HTML-T1',
-  mapping: 'H24',
+  name: 'Using the link element and navigation tools',
+  code: 'QW-HTML-T19',
+  mapping: 'H59',
   description: 'The objective of this technique is to describe how the link element can provide metadata about the position of an HTML page within a set of Web pages or can assist in locating content with a set of Web pages.',
   metadata: {
     target: {
-      'parent-sibling': 'img',
-      parent: 'map',
-      element: 'area'
+      element: 'link'
     },
     'success-criteria': [{
         name: '2.4.5',
@@ -64,10 +62,6 @@ class QW_HTML_T19 extends Technique {
 
     const parent = await DomUtils.getElementParent(element);
 
-    if (!parent) {
-      return;
-    }
-
     const evaluation: HTMLTechniqueResult = {
       verdict: '',
       description: '',
@@ -77,28 +71,37 @@ class QW_HTML_T19 extends Technique {
     const parentName = await DomUtils.getElementTagName(parent);
 
     if (parentName !== 'head') {
-      evaluation.verdict = 'failed';
-      evaluation.description = `The element is not contained in the head element`;
+      evaluation.verdict = 'warning';
+      evaluation.description = `The element is not contained in the head element. Verify if this link is used for navigation, and if it is, it must be inside the <head>`;
       evaluation.resultCode = 'RC1';
     } else if (!(await DomUtils.elementHasAttributes(element))) { // fails if the element doesn't contain an alt attribute
-      evaluation.verdict = 'failed';
+      evaluation.verdict = 'inapplicable';
       evaluation.description = `The element doesn't contain a rel or an href attribute`;
       evaluation.resultCode = 'RC2';
     } else {
-      const rel = await DomUtils.getElementAttribute(element, 'rel');
-      const href = await DomUtils.getElementAttribute(element, 'href');
+      //const rel = await DomUtils.getElementAttribute(element, 'rel');
+      //const href = await DomUtils.getElementAttribute(element, 'href');
 
-      if (!rel) {
-        evaluation.verdict = 'warning';
-        evaluation.description = `The element doesn't contain a rel attribute check if this element pertains navigation `;
+      const [rel, href] = await Promise.all([
+        DomUtils.getElementAttribute(element, 'rel'),
+        DomUtils.getElementAttribute(element, 'href')
+      ]);
+
+      const relNavigationValues = ['alternate', 'author', 'help', 'license', 'next', 'prev', 'search'];
+
+      const relForNavigation = rel && relNavigationValues.includes(rel);
+
+      if (!relForNavigation) {
+        evaluation.verdict = 'inapplicable';
+        evaluation.description = `The element doesn't contain a rel attribute or doesn't pertains navigation`;
         evaluation.resultCode = 'RC3';
       } else if (!href) {
-        evaluation.verdict = 'warning';
-        evaluation.description = `The element doesn't contain an href attribute check if this element pertains navigation`;
+        evaluation.verdict = 'failed';
+        evaluation.description = `The element doesn't contain an href attribute and pertains navigation`;
         evaluation.resultCode = 'RC4';
       } else {
-        evaluation.verdict = 'warning';
-        evaluation.description = 'The element contains a rel and an href attribute that should be manually verified';
+        evaluation.verdict = 'passed';
+        evaluation.description = 'The link has rel and href attributes and pertains navigation';
         evaluation.resultCode = 'RC5';
       }
     }
