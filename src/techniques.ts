e5701069854def3dc96d61ee18ee49d7851e1ace
aestriga <aestriga@lasige.di.fc.ul.pt>
2019-12-06 18:53:51 +0000

    code done

--- src/techniques.ts
+++ src/techniques.ts
@@ -32,6 +32,7 @@ import QW_HTML_T33 from './techniques/QW-HTML-T33';
 import QW_HTML_T34 from './techniques/QW-HTML-T34';
 import QW_HTML_T35 from './techniques/QW-HTML-T35';
 import QW_HTML_T37 from './techniques/QW-HTML-T37';
+import QW_HTML_T38 from './techniques/QW-HTML-T38';
 import QW_HTML_T39 from './techniques/QW-HTML-T39';
 import QW_HTML_T40 from './techniques/QW-HTML-T40';
 import QW_HTML_T41 from './techniques/QW-HTML-T41';
@@ -72,6 +73,7 @@ const techniques = {
   'QW-HTML-T34': new QW_HTML_T34(),
   'QW-HTML-T35': new QW_HTML_T35(),
   'QW-HTML-T37': new QW_HTML_T37(),
+  'QW-HTML-T38': new QW_HTML_T38(),
   'QW-HTML-T39': new QW_HTML_T39(),
   'QW-HTML-T40': new QW_HTML_T40(),
   'QW-HTML-T41': new QW_HTML_T41(),
@@ -116,6 +118,7 @@ const techniquesToExecute = {
   'QW-HTML-T33': true,
   'QW-HTML-T35': true,
   'QW-HTML-T37': true,
+  'QW-HTML-T38': true,
   'QW-HTML-T39': true,
   'QW-HTML-T40': true,
   'QW-HTML-T41': true,
